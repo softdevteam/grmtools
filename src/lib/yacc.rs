d771b0446374af0e68401672f7224973d94289d5
Lukas Diekmann <lukas.diekmann@gmail.com>
2015-08-26 11:27:59 +0100

    adjust rule names to YACC specification

--- src/lib/yacc.rs
+++ src/lib/yacc.rs
@@ -149,7 +149,7 @@ impl YaccParser {
     }
 
     fn parse_name(&self, i: usize) -> YaccResult<(usize, String)> {
-        let re = Regex::new(r"^[^\t\n\v\f\r :;|]+").unwrap();
+        let re = Regex::new(r"^[a-zA-Z_.][a-zA-Z0-9_.]*").unwrap();
         match re.find(&self.src[i..]) {
             Some((s, e)) => {
                 assert!(s == 0);
