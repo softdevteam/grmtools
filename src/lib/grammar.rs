de5ee7d1d6494bd62ff4bddb17d47d74026082ac
Laurence Tratt <laurie@tratt.net>
2015-09-16 14:52:14 +0059

    Move tests to within their source files.
    It's my fault that we didn't do this in the first place. It meant that we had to
    expose nearly every method as public, even when we really didn't want to. Mea
    culpa.

--- src/lib/grammar.rs
+++ src/lib/grammar.rs
@@ -25,7 +25,6 @@ pub struct Grammar {
     pub end_term: TIdx,
     pub rules_alts: Vec<Vec<AIdx>>,
     pub alts: Vec<Vec<Symbol>>,
-    //pub alts_rules: HashMap<AIdx, NIdx>
 }
 
 #[derive(Clone, Debug, Hash, Eq, PartialEq)]
@@ -34,6 +33,7 @@ pub enum Symbol {
     Terminal(TIdx)
 }
 
+#[cfg(test)]
 impl Grammar {
     // For testing purposes only
     pub fn nonterminal_off(&self, n: &str) -> AIdx {
@@ -68,7 +68,7 @@ impl Grammar {
 ///   ^ : S '$';
 pub fn ast_to_grammar(ast: &grammar_ast::GrammarAST) -> Grammar {
     // The user is expected to have called validate before calling this function.
-    assert!(ast.validate().is_ok());
+    debug_assert!(ast.validate().is_ok());
 
     // First of all generate guaranteed unique start nonterm and end term names. We simply keep
     // making the string longer until we've hit something unique (at the very worst, this will
@@ -137,5 +137,81 @@ pub fn ast_to_grammar(ast: &grammar_ast::GrammarAST) -> Grammar {
         end_term:          terminal_map[&end_term],
         rules_alts:        rules_alts,
         alts:              alts,
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::{ast_to_grammar, Symbol};
+    use yacc::parse_yacc;
+
+    #[test]
+    fn test_minimal() {
+        let ast = parse_yacc(&"%start R %token T %% R: 'T';".to_string()).unwrap();
+        let grm = ast_to_grammar(&ast);
+
+        assert_eq!(grm.start_alt, 0);
+        grm.nonterminal_off("^");
+        grm.nonterminal_off("R");
+        grm.terminal_off("$");
+        grm.terminal_off("T");
+
+        assert_eq!(grm.rules_alts, vec![vec![0], vec![1]]);
+        let start_alt = grm.alts.get(grm.rules_alts.get(grm.nonterminal_off("^")).unwrap()[0]).unwrap();
+        assert_eq!(*start_alt, vec![Symbol::Nonterminal(grm.nonterminal_off("R"))]);
+        let r_alt = grm.alts.get(grm.rules_alts.get(grm.nonterminal_off("R")).unwrap()[0] as
+                                 usize).unwrap();
+        assert_eq!(*r_alt, vec![Symbol::Terminal(grm.terminal_off("T"))]);
+    }
+
+    #[test]
+    fn test_rule_ref() {
+        let ast = parse_yacc(&"%start R %token T %% R : S; S: 'T';".to_string()).unwrap();
+        let grm = ast_to_grammar(&ast);
+
+        grm.nonterminal_off("^");
+        grm.nonterminal_off("R");
+        grm.nonterminal_off("S");
+        grm.terminal_off("$");
+        grm.terminal_off("T");
+
+        assert_eq!(grm.rules_alts, vec![vec![0], vec![1], vec![2]]);
+        let start_alt = grm.alts.get(grm.rules_alts.get(grm.nonterminal_off("^")).unwrap()[0]).unwrap();
+        assert_eq!(*start_alt, vec![Symbol::Nonterminal(grm.nonterminal_off("R"))]);
+        let r_alt = grm.alts.get(grm.rules_alts.get(grm.nonterminal_off("R")).unwrap()[0] as
+                                 usize).unwrap();
+        assert_eq!(r_alt.len(), 1);
+        assert_eq!(r_alt[0], Symbol::Nonterminal(grm.nonterminal_off("S")));
+        let s_alt = grm.alts.get(grm.rules_alts.get(grm.nonterminal_off("S")).unwrap()[0] as
+                                 usize).unwrap();
+        assert_eq!(s_alt.len(), 1);
+        assert_eq!(s_alt[0], Symbol::Terminal(grm.terminal_off("T")));
+    }
+
+    #[test]
+    fn test_long_alt() {
+        let ast = parse_yacc(&"%start R %token T1 T2 %% R : S 'T1' S; S: 'T2';".to_string()).unwrap();
+        let grm = ast_to_grammar(&ast);
+
+        grm.nonterminal_off("^");
+        grm.nonterminal_off("R");
+        grm.nonterminal_off("S");
+        grm.terminal_off("$");
+        grm.terminal_off("T1");
+        grm.terminal_off("T2");
+
+        assert_eq!(grm.rules_alts, vec![vec![0], vec![1], vec![2]]);
+        let start_alt = grm.alts.get(grm.rules_alts.get(grm.nonterminal_off("^")).unwrap()[0]).unwrap();
+        assert_eq!(*start_alt, vec![Symbol::Nonterminal(grm.nonterminal_off("R"))]);
+        let r_alt = grm.alts.get(grm.rules_alts.get(grm.nonterminal_off("R")).unwrap()[0] as
+                                 usize).unwrap();
+        assert_eq!(r_alt.len(), 3);
+        assert_eq!(r_alt[0], Symbol::Nonterminal(grm.nonterminal_off("S")));
+        assert_eq!(r_alt[1], Symbol::Terminal(grm.terminal_off("T1")));
+        assert_eq!(r_alt[2], Symbol::Nonterminal(grm.nonterminal_off("S")));
+        let s_alt = grm.alts.get(grm.rules_alts.get(grm.nonterminal_off("S")).unwrap()[0] as
+                                 usize).unwrap();
+        assert_eq!(s_alt.len(), 1);
+        assert_eq!(s_alt[0], Symbol::Terminal(grm.terminal_off("T2")));
     }
 }
--- src/lib/grammar_ast.rs
+++ src/lib/grammar_ast.rs
@@ -168,13 +168,106 @@ impl PartialEq for Rule {
     }
 }
 
-/// Returns a nonterminal symbol with name `n`.
-pub fn nonterminal(n: &str) -> Symbol {
-    Symbol::Nonterminal(n.to_string())
-}
+#[cfg(test)]
+mod test {
+    use super::{GrammarAST, GrammarASTError, GrammarASTErrorKind, Symbol};
 
-/// Returns a terminal symbol with name `n`.
-pub fn terminal(n: &str) -> Symbol {
-    Symbol::Terminal(n.to_string())
-}
+    fn nonterminal(n: &str) -> Symbol {
+        Symbol::Nonterminal(n.to_string())
+    }
 
+    fn terminal(n: &str) -> Symbol {
+        Symbol::Terminal(n.to_string())
+    }
+
+    #[test]
+    fn test_empty_grammar(){
+        let grm = GrammarAST::new();
+        match grm.validate() {
+            Err(GrammarASTError{kind: GrammarASTErrorKind::NoStartRule, ..}) => (),
+            _ => panic!("Validation error")
+        }
+    }
+
+    #[test]
+    fn test_invalid_start_rule(){
+        let mut grm = GrammarAST::new();
+        grm.start = Some("A".to_string());
+        grm.add_rule("B".to_string(), vec!());
+        match grm.validate() {
+            Err(GrammarASTError{kind: GrammarASTErrorKind::InvalidStartRule, ..}) => (),
+            _ => panic!("Validation error")
+        }
+    }
+
+    #[test]
+    fn test_valid_start_rule(){
+        let mut grm = GrammarAST::new();
+        grm.start = Some("A".to_string());
+        grm.add_rule("A".to_string(), vec!());
+        assert!(grm.validate().is_ok());
+    }
+
+    #[test]
+    fn test_valid_nonterminal_ref(){
+        let mut grm = GrammarAST::new();
+        grm.start = Some("A".to_string());
+        grm.add_rule("A".to_string(), vec!(nonterminal("B")));
+        grm.add_rule("B".to_string(), vec!());
+        assert!(grm.validate().is_ok());
+    }
+
+    #[test]
+    fn test_invalid_nonterminal_ref(){
+        let mut grm = GrammarAST::new();
+        grm.start = Some("A".to_string());
+        grm.add_rule("A".to_string(), vec!(nonterminal("B")));
+        match grm.validate() {
+            Err(GrammarASTError{kind: GrammarASTErrorKind::UnknownRuleRef, ..}) => (),
+            _ => panic!("Validation error")
+        }
+    }
+
+    #[test]
+    fn test_valid_terminal_ref(){
+        let mut grm = GrammarAST::new();
+        grm.tokens.insert("b".to_string());
+        grm.start = Some("A".to_string());
+        grm.add_rule("A".to_string(), vec!(terminal("b")));
+        assert!(grm.validate().is_ok());
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_valid_token_ref(){
+        // for now we won't support the YACC feature that allows
+        // to redefine nonterminals as tokens by adding them to '%token'
+        let mut grm = GrammarAST::new();
+        grm.tokens.insert("b".to_string());
+        grm.start = Some("A".to_string());
+        grm.add_rule("A".to_string(), vec!(nonterminal("b")));
+        assert!(grm.validate().is_ok());
+    }
+
+    #[test]
+    fn test_invalid_terminal_ref(){
+        let mut grm = GrammarAST::new();
+        grm.start = Some("A".to_string());
+        grm.add_rule("A".to_string(), vec!(terminal("b")));
+        match grm.validate() {
+            Err(GrammarASTError{kind: GrammarASTErrorKind::UnknownToken, ..}) => (),
+            _ => panic!("Validation error")
+        }
+    }
+
+    #[test]
+    fn test_invalid_nonterminal_forgotten_token(){
+        let mut grm = GrammarAST::new();
+        grm.start = Some("A".to_string());
+        grm.add_rule("A".to_string(), vec!(nonterminal("b"), terminal("b")));
+        match grm.validate() {
+            Err(GrammarASTError{kind: GrammarASTErrorKind::UnknownRuleRef, ..}) => (),
+            _ => panic!("Validation error")
+        }
+    }
+}
