ae60035f4b5727e4299c60eae3f6245919e97cc2
Laurence Tratt <laurie@tratt.net>
2018-08-14 16:47:47 +0100

    Document, and sort-of enforce, the constraints on integers we expect.
    Rust imposes surprisingly few constraints on integers. That makes it very hard
    to deal with code that uses integers of different sizes. For example, u8 and u16
    can be converted infallibly to usize, but u32 and bigger can only be fallibly
    converted (in nightly) to usize (i.e. using try_from). That means that usize
    can't be converted infallibly to u32 or bigger, so code which assumes safety
    between u32 and usize is not guaranteed to be portable.
    
    I have tried 4 or 5 of ways of working around this, and nothing has been
    satisfactory. This commit enforces the following constraints on the integer
    types that cfgrammar will work with:
    
      * StorageT can be infallibly converted to usize
      * usize can be infallibly converted to StorageT
    
    if either direction were to lead to a loss of precision, a panic occurs. [It
    would be nice to have this happen at compile-time, but I haven't been able to
    get that to work properly.]
    
    Is this perfect? Clearly not. It means that our code is less portable than
    normal Rust code if StorageT > u16. However, the user is in control of this, and
    it means we can simplify our code base and reduce the chances that we're bitten
    by nasty errors.

--- cfgrammar/src/lib/idxnewtype.rs
+++ cfgrammar/src/lib/idxnewtype.rs
@@ -33,6 +33,7 @@
 // This macro generates a struct which exposes a u32 API (but which may, internally, use a smaller
 // storage size).
 
+use std::mem::size_of;
 
 use num_traits::{self, PrimInt, Unsigned};
 
@@ -57,12 +58,14 @@ macro_rules! IdxNewtype {
 
         impl<T: PrimInt + Unsigned> From<$n<T>> for usize {
             fn from(st: $n<T>) -> Self {
+                debug_assert!(size_of::<usize>() >= size_of::<T>());
                 num_traits::cast(st.0).unwrap()
             }
         }
 
-        impl<T: PrimInt + Unsigned> From<$n<T>> for u32 where T: Unsigned {
+        impl<T: PrimInt + Unsigned> From<$n<T>> for u32 {
             fn from(st: $n<T>) -> Self {
+                debug_assert!(size_of::<u32>() >= size_of::<T>());
                 num_traits::cast(st.0).unwrap()
             }
         }
@@ -77,14 +80,30 @@ macro_rules! IdxNewtype {
 
 IdxNewtype!(
     /// A type specifically for nonterminal indices.
+    ///
+    /// It is guaranteed that `NTIdx` can be converted, without loss of precision, to `usize` with
+    /// the idiom `NTIdx::from(x_usize)`. `usize` values can be converted to `NTIdx`, causing a
+    /// panic if this would lead to a loss of precision with `usize::from(y_ntidx)`.
     NTIdx);
 IdxNewtype!(
     /// A type specifically for production indices (e.g. a rule `E::=A|B` would
     /// have two productions for the single rule `E`).
+    ///
+    /// It is guaranteed that `PIdx` can be converted, without loss of precision, to `usize` with
+    /// the idiom `PIdx::from(x_usize)`. `usize` values can be converted to `PTIdx`, causing a
+    /// panic if this would lead to a loss of precision with `usize::from(y_pidx)`.
     PIdx);
 IdxNewtype!(
     /// A type specifically for symbol indices (within a production).
+    ///
+    /// It is guaranteed that `SIdx` can be converted, without loss of precision, to `usize` with
+    /// the idiom `SIdx::from(x_usize)`. `usize` values can be converted to `NTIdx`, causing a
+    /// panic if this would lead to a loss of precision with `usize::from(y_sidx)`.
     SIdx);
 IdxNewtype!(
     /// A type specifically for token indices.
+    ///
+    /// It is guaranteed that `TIdx` can be converted, without loss of precision, to `usize` with
+    /// the idiom `TIdx::from(x_usize)`. `usize` values can be converted to `TIdx`, causing a
+    /// panic if this would lead to a loss of precision with `usize::from(y_tidx)`.
     TIdx);
