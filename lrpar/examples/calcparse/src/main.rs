7715d36cb3d5e148088b10822ce640b2e1873d5f
Laurence Tratt <laurie@tratt.net>
2018-08-23 19:55:36 +0100

    Add a simple evaluator for the calculator.
    This allows you to type in things like "2+3*4" and have them evaluated (in this
    case as 14). At the moment this has to be done in a slightly verbose manner.
    Making things less verbose is a job for the future.

--- lrpar/examples/calcparse/src/main.rs
+++ lrpar/examples/calcparse/src/main.rs
@@ -1,7 +1,11 @@
 use std::io::{self, BufRead, Write};
 
+extern crate cfgrammar;
 #[macro_use] extern crate lrlex;
 #[macro_use] extern crate lrpar;
+
+use cfgrammar::NTIdx;
+use lrpar::Node;
 
 // Using `lrlex_mod!` brings the lexer for `calc.l` into scope.
 lrlex_mod!(calc_l);
@@ -27,7 +31,7 @@ fn main() {
                     Ok(lexemes) => {
                         // Now parse the stream of lexemes into a tree
                         match calc_y::parse(&lexemes) {
-                            Ok(pt) => println!("{:?}", pt),
+                            Ok(pt) => println!("{}", Eval::new(l).eval(&pt)),
                             Err((_, errs)) => {
                                 // One or more errors were detected during parsing.
                                 for e in errs {
@@ -42,6 +46,50 @@ fn main() {
                 }
             },
             _ => break
+        }
+    }
+}
+
+struct Eval<'a> {
+    s: &'a str
+}
+
+impl<'a> Eval<'a> {
+    fn new(s: &'a str) -> Self {
+        Eval{s}
+    }
+
+    fn eval(&self, n: &Node<u8>) -> i64 {
+        match *n {
+            Node::Nonterm{nonterm_idx: NTIdx(ntidx), ref nodes} if ntidx==NT_EXPR => {
+                if nodes.len() == 1 {
+                    self.eval(&nodes[0])
+                } else {
+                    debug_assert_eq!(nodes.len(), 3);
+                    self.eval(&nodes[0]) + self.eval(&nodes[2])
+                }
+            },
+            Node::Nonterm{nonterm_idx: NTIdx(ntidx), ref nodes} if ntidx==NT_TERM => {
+                if nodes.len() == 1 {
+                    self.eval(&nodes[0])
+                } else {
+                    debug_assert_eq!(nodes.len(), 3);
+                    self.eval(&nodes[0]) * self.eval(&nodes[2])
+                }
+            },
+            Node::Nonterm{nonterm_idx: NTIdx(ntidx), ref nodes} if ntidx==NT_FACTOR => {
+                if nodes.len() == 1 {
+                    if let Node::Term{lexeme} = nodes[0] {
+                        self.s[lexeme.start()..lexeme.end()].parse().unwrap()
+                    } else {
+                        unreachable!();
+                    }
+                } else {
+                    debug_assert_eq!(nodes.len(), 3);
+                    self.eval(&nodes[1])
+                }
+            },
+            _ => unreachable!()
         }
     }
 }
