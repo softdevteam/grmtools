18d1e87601e6a49628a4432860602cc2d329f3c6
Laurence Tratt <laurie@tratt.net>
2020-02-08 15:13:11 +0000

    Make use of the span var to report nicer error messages.
    For example, this input:
    
      1+9223372036854775808+9223372036854775808+9223372036854775808
    
    previously lead to the output:
    
      thread 'main' panicked at 'attempt to add with overflow', lrpar/examples/calc_ast/src/main.rs:49:38
    
    but now leads to:
    
      Column 23, '9223372036854775808+9223372036854775808' overflowed.

--- lrpar/examples/calc_ast/src/calc.y
+++ lrpar/examples/calc_ast/src/calc.y
@@ -2,30 +2,37 @@
 %avoid_insert "INT"
 %%
 Expr -> Result<Expr<'input>, ()>:
-      Term '+' Expr { Ok(Expr::Add{lhs: Box::new($1?), rhs: Box::new($3?)}) }
+      Term '+' Expr { Ok(Expr::Add{span: $span, lhs: Box::new($1?), rhs: Box::new($3?)}) }
     | Term { $1 }
     ;
 
 Term -> Result<Expr<'input>, ()>:
-      Factor '*' Term { Ok(Expr::Mul{lhs: Box::new($1?), rhs: Box::new($3?)}) }
+      Factor '*' Term { Ok(Expr::Mul{span: $span, lhs: Box::new($1?), rhs: Box::new($3?)}) }
     | Factor { $1 }
     ;
 
 Factor -> Result<Expr<'input>, ()>:
       '(' Expr ')' { $2 }
-    | 'INT' { Ok(Expr::Number($lexer.lexeme_str(&$1.map_err(|_| ())?))) }
+    | 'INT' { Ok(Expr::Number{span: $span, int: $lexer.lexeme_str(&$1.map_err(|_| ())?) }) }
     ;
 %%
+
+use lrpar::Span;
 
 #[derive(Debug)]
 pub enum Expr<'input> {
     Add {
+        span: Span,
         lhs: Box<Expr<'input>>,
         rhs: Box<Expr<'input>>,
     },
     Mul {
+        span: Span,
         lhs: Box<Expr<'input>>,
         rhs: Box<Expr<'input>>,
     },
-    Number(&'input str)
+    Number {
+        span: Span,
+        int: &'input str
+    }
 }
--- lrpar/examples/calc_ast/src/main.rs
+++ lrpar/examples/calc_ast/src/main.rs
@@ -1,7 +1,7 @@
 use std::io::{self, BufRead, Write};
 
 use lrlex::lrlex_mod;
-use lrpar::lrpar_mod;
+use lrpar::{lrpar_mod, Lexer, Span};
 
 // Using `lrlex_mod!` brings the lexer for `calc.l` into scope. By default the module name will be
 // `calc_l` (i.e. the file name, minus any extensions, with a suffix of `_l`).
@@ -32,28 +32,36 @@ fn main() {
                     println!("{}", e.pp(&lexer, &calc_y::token_epp));
                 }
                 if let Some(Ok(r)) = res {
-                    if let Ok(i) = eval(r) {
-                        println!("Result: {}", i);
-                        continue;
+                    match eval(&lexer, r) {
+                        Ok(i) => println!("Result: {}", i),
+                        Err((span, msg)) => {
+                            let ((line, col), _) = lexer.line_col(span);
+                            eprintln!(
+                                "Evaluation error at line {} column {}, '{}' {}.",
+                                line,
+                                col,
+                                lexer.span_str(span),
+                                msg
+                            )
+                        }
                     }
                 }
-                eprintln!("Unable to evaluate expression.");
             }
             _ => break
         }
     }
 }
 
-fn eval(e: Expr) -> Result<u64, ()> {
+fn eval(lexer: &dyn Lexer<u32>, e: Expr) -> Result<u64, (Span, &'static str)> {
     match e {
-        Expr::Add { lhs, rhs } => Ok(eval(*lhs)? + eval(*rhs)?),
-        Expr::Mul { lhs, rhs } => Ok(eval(*lhs)? * eval(*rhs)?),
-        Expr::Number(s) => match s.parse::<u64>() {
-            Ok(i) => Ok(i),
-            Err(_) => {
-                eprintln!("{} cannot be represented as a u64", s);
-                Err(())
-            }
-        }
+        Expr::Add { span, lhs, rhs } => eval(lexer, *lhs)?
+            .checked_add(eval(lexer, *rhs)?)
+            .ok_or((span, "overflowed")),
+        Expr::Mul { span, lhs, rhs } => eval(lexer, *lhs)?
+            .checked_mul(eval(lexer, *rhs)?)
+            .ok_or((span, "overflowed")),
+        Expr::Number { span, int } => int
+            .parse::<u64>()
+            .map_err(|_| (span, "cannot be represented as a u64"))
     }
 }
