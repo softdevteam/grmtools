abc5f0a346901c1a92765a5bf5ac71c039fa7b32
matt rice <ratmice@gmail.com>
2024-01-02 13:20:35 -0800

    Add SimpleReport to impl ErrorReport

--- cfgrammar/src/lib/yacc/reporting.rs
+++ cfgrammar/src/lib/yacc/reporting.rs
@@ -7,3 +7,33 @@ pub trait ErrorReport {
     /// Gives ownership of a warning to self.
     fn grammar_warning(&mut self, err: YaccGrammarWarning);
 }
+
+/// A basic Report that stores errors and warnings in a `Vec`.
+pub struct SimpleReport {
+    errors: Vec<YaccGrammarError>,
+    warnings: Vec<YaccGrammarWarning>,
+}
+
+impl ErrorReport for SimpleReport {
+    fn grammar_error(&mut self, err: YaccGrammarError) {
+        self.errors.push(err);
+    }
+    fn grammar_warning(&mut self, err: YaccGrammarWarning) {
+        self.warnings.push(err);
+    }
+}
+
+impl SimpleReport {
+    pub fn new() -> Self {
+        SimpleReport {
+            errors: vec![],
+            warnings: vec![],
+        }
+    }
+    pub(crate) fn warnings(&self) -> &[YaccGrammarWarning] {
+        &self.warnings
+    }
+    pub(crate) fn errors(&self) -> &[YaccGrammarError] {
+        &self.errors
+    }
+}
