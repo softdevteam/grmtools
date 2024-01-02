5a6fc5bbba3408e7d822f15307173afde38ae2aa
matt rice <ratmice@gmail.com>
2024-01-02 15:52:56 -0800

    Add ASTBuilderError

--- cfgrammar/src/lib/yacc/reporting.rs
+++ cfgrammar/src/lib/yacc/reporting.rs
@@ -1,4 +1,5 @@
 use super::{YaccGrammarError, YaccGrammarWarning};
+use std::fmt;
 
 /// Caller gives ownership of errors and warnings to the impl.
 pub trait ErrorReport {
@@ -35,5 +36,21 @@ impl SimpleReport {
     }
     pub(crate) fn errors(&self) -> &[YaccGrammarError] {
         &self.errors
+    }
+}
+#[derive(Debug)]
+pub enum ASTBuilderError {
+    /// This error is just an indication that a failure occurred.
+    /// But otherwise lacks information such as what and where.
+    /// For that you will need to look at a `YaccGrammarError`
+    /// in an `ErrorReport`.
+    ConstructionFailure,
+}
+
+impl fmt::Display for ASTBuilderError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        match self {
+            Self::ConstructionFailure => write!(f, "Grammar construction failure"),
+        }
     }
 }
