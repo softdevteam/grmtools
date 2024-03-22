eba74050bddff14cb08327670150a8154dc0b192
github_hooks <noreply@soft-dev.org>
2024-03-22 10:54:52 +0000

    Deploy gh-pages

---
+++ master/api/trait.impl/cfgrammar/span/trait.Spanned.js
@@ -0,0 +1,4 @@
+(function() {var implementors = {
+"cfgrammar":[],
+"lrlex":[["impl <a class=\"trait\" href=\"cfgrammar/span/trait.Spanned.html\" title=\"trait cfgrammar::span::Spanned\">Spanned</a> for <a class=\"struct\" href=\"lrlex/struct.LexBuildError.html\" title=\"struct lrlex::LexBuildError\">LexBuildError</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
