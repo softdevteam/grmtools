9f963cbcc54bad03c9c88e4dd738934579e13679
github_hooks <noreply@soft-dev.org>
2024-12-22 10:13:02 +0000

    Deploy gh-pages

---
+++ master/api/trait.impl/lrpar/lex_api/trait.LexError.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["lrlex",[["impl <a class=\"trait\" href=\"lrpar/lex_api/trait.LexError.html\" title=\"trait lrpar::lex_api::LexError\">LexError</a> for <a class=\"struct\" href=\"lrlex/struct.LRLexError.html\" title=\"struct lrlex::LRLexError\">LRLexError</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[248]}
